b362ce865d0205e23096be35752d4c0a25c48307
rscarth <ryland.scarth233@gmail.com>
2021-08-22 00:41:26 -0700

    Reformating of cart module

--- src/views/order/Cart.vue
+++ src/views/order/Cart.vue
@@ -5,58 +5,50 @@
         <div style="float: right">{{ totalCount }} {{ totalCount == 1 ? "Item" : "Items"}}</div>
     </div>
     <div id="cart-body">
-        <ul>
-            <li>
-                <div class="cart-item" style="font-weight: bold; text-decoration: underline;">
-                    <span class="item-name">Name</span>
-                    <span class="quantity-holder">Quantity</span>
-                    <span class="item-price">Price</span>
-                    <span class="item-price">Total</span>
-                </div>
-            </li>
-            <li v-for="item in cartData" :key="item.id">
-                <div class="cart-item">
-                    <div class="item-name">{{ item.name }}</div>
-                    <div class="quantity-holder">
-                        <button class="quantity-button" v-on:click="changeQuantity(item.quantity-1, item)">-</button>
-                        <input type="number" class="quantity-counter" :value="item.quantity" v-on:keyup.enter="changeQuantity($event.target.valueAsNumber, item)">
-                        <button class="quantity-button" v-on:click="changeQuantity(item.quantity+1, item)">+</button>
-                    </div>
-                    <div class="item-price">${{ (item.price*0.01).toFixed(2) }}</div>
-                    <div class="item-price">${{((item.price*0.01)*item.quantity).toFixed(2)}}</div>
-                </div>
-            </li>
-        </ul>
-        <div :style="[ cartData.length == 0 ? {'display': 'none'} : {} ]">Total: ${{ (price*0.01).toFixed(2) }}</div>
+        <table id="cart">
+                <CartItem v-for="item in cartData" :key="item.id" :item="item" v-on:change-quantity="$emit('change-quantity')"/>
+        </table>
     </div>
-    <div id="cart-checkout" :style="[ cartData.length == 0 ? {'display': 'none'} : {} ]">
-        <button>Place Order</button>
+    <div id="cart-checkout" v-if="cartData.length != 0">
+        <div id="cart-checkout__total">Total: ${{ (price*0.01).toFixed(2) }}</div>
+        <button id="cart-checkout__button" v-on:click="createOrder">Place Order</button>
     </div>
 </div>
 </template>
 
 <script>
 import { cartData } from '@/components/cart';
+import createOrder from '@/components/createOrder';
+
+import CartItem from '@/views/order/CartItem.vue';
 
 export default {
     name: 'Cart',
+    components: {
+        CartItem
+    },
     data () {
         return {
             cartData: cartData.data,
             price: cartData.totalPrice,
-            totalCount: cartData.totalCount 
+            totalCount: cartData.totalCount
         }
     },
     beforeMount(){
         // console.log(cartData);
     },
     beforeUpdate(){
+        console.log(this.totalCount, cartData.totalCount)
+        console.log(this.price, cartData.totalPrice)
         this.totalCount = cartData.totalCount
         this.price = cartData.totalPrice
     },
     methods: {
-        changeQuantity(n, item) {
-            this.$emit("item-update", n, item);
+        editCartItem(item){
+            console.log(item)
+        },
+        createOrder() {
+            createOrder()
         }
     }
 }
@@ -66,14 +58,14 @@ export default {
 #cart-holder {
     flex: 1;
     box-shadow: 0 0 3px #ccc;
-    padding: 0 30px 0 30px;
 }
 
 #cart-header {
-    width: 100%;
+    width: 100%-40px;
     height: 60px;
-    border-bottom: 1px solid #ccc;
     margin-bottom: 10px;
+    margin: 0 20px;
+    /* border-bottom: 1px solid #ccc; */
 }
 
 #cart-header > div {
@@ -84,54 +76,41 @@ export default {
 }
 
 #cart-body {
+    margin-top: 0px;
     width: 100%;
+    text-align: left;
     font-size: 18px;
-    font-weight: normal;
+}
+
+#cart {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+#cart-checkout {
+    margin-top: 10px;
+}
+
+#cart-checkout__total {
+    width: auto;
+}
+
+#cart-checkout__button {
+    margin-top: 10px;
+    border-radius: 30px;
+    border: 0px;
+    padding: 5px 30px;
+    background-color: #42b983;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
     letter-spacing: -1px;
 }
 
-.cart-item {
-    width: 100%;
-    height: auto;
-    display: inline-flex;
-    align-items: center;
-    line-height: 1em;
-    text-align: center;
-    margin-bottom: 10px;
+#cart-checkout__button:hover {
+    padding: 5px 40px;
+    transform: scale(1.05);
+    transition: 0.25s;
 }
-
-.cart-item > * {
-    flex: 1 1 0;
-}
-
-.item-name {
-    flex: 0 0 30%;
-    text-align: center;
-}
-
-.quantity-holder {
-    height: 20px;
-    display: inline-flex;
-}
-
-.quantity-holder > * {
-    margin: 0 1px 0 1px;
-}
-
-.quantity-button {
-    width: 30%;
-}
-
-.quantity-counter {
-    width: 40%;
-    text-align: center;
-}
-
-.item-price {
-
-}
-
-/* #cart-body > ul > li:first-child > div.cart-item {
-    padding-top: 0;
-} */
 </style>
---
+++ src/views/order/CartItem.vue
@@ -0,0 +1,91 @@
+<template>
+<tr>
+    <td class="cart__quantity"><span>{{ item.quantity }}</span></td>
+    <td class="cart-center">
+        <span>{{ item.name }}</span>
+        <div>
+            <button class="cart-center__button hover--darker-green hover--underline"
+                    v-on:click="edit = true">Edit</button>
+            <button class="cart-center__button hover--darker-green hover--underline"
+                    v-on:click="deleteCartItem()">Remove</button>
+        </div>
+        <QuantityEdit v-if="edit"
+                      :item="item"
+                      v-on:change-quantity="changeQuantity"
+                      v-on:close-edit="edit = false"/>
+    </td>
+    <td class="cart__total"><div>${{((item.price*0.01)*item.quantity).toFixed(2)}}</div></td>
+</tr>
+</template>
+
+<script>
+import QuantityEdit from '@/views/order/QuantityEdit.vue';
+
+import { deleteItem as cartDeleteItem } from '@/components/cart';
+
+export default {
+    name: 'CartItem',
+    props: ['item'],
+    components: {
+        QuantityEdit
+    },
+    data() {
+        return {
+            edit: false
+        }
+    },
+    methods: {
+        changeQuantity() {
+            this.edit = false;
+            console.log("test")
+            this.$emit('change-quantity')
+        },
+        deleteCartItem(){
+            cartDeleteItem(this.item);
+        }
+    }
+}
+</script>
+
+<style>
+#cart tr {
+    cursor: pointer;
+}
+
+#cart tr:hover {
+    background-color: #dfdfdf;
+}
+
+#cart td {
+    padding: 15px 0px 15px;
+    vertical-align: top;
+}
+
+.cart__quantity {
+    width: 10%;
+    font-weight: bold;
+}
+
+.cart__quantity span {
+    padding-left: 20px;
+}
+
+.cart-center {
+    font-weight: bold;
+}
+.cart-center__button {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    padding: 1px 0;
+    margin-right: 10px;
+    font-size: 14px;
+    font-weight: bolder;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #42b983;
+}
+
+.cart__total {
+    width: 20%;
+}
+</style>
